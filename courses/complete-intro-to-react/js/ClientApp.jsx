4d178972adda18b7257e4440eedad056ef81975b
johntduong <johntduong@yahoo.com>
2018-01-04 23:59:37 -0800

    added prettier and modified structure

---
+++ courses/complete-intro-to-react/js/ClientApp.jsx
@@ -0,0 +1,17 @@
+const ce = React.createElement;
+const MyTitle = props => {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+
+const MyFirstComponent = () => {
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'The Last Kingdom', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Legion', color: 'Peru' })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));

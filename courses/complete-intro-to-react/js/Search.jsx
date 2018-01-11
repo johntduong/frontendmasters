a9a2b28fcf13e67cf8fa4aa3f7b2bef026c37354
johntduong <johntduong@yahoo.com>
2018-01-11 13:17:20 -0800

    rewriting Search component to ES6 class

--- courses/complete-intro-to-react/js/Search.jsx
+++ courses/complete-intro-to-react/js/Search.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+// Stateless functional component
+// const Search = () => (
+//   <div className="search">
+//   <header>
+//     <h1>svideo</h1>
+//     <input type="text" placeholder="search" />
+//   </header>
+//     <div>
+//       {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+//     </div>
+//   </div>
+// );
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: "this is some sort of debug statement"
+    };
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input type="text" placeholder="search" />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;

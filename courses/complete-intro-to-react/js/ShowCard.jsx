ba02cf3e0c5f7890c29dcb92112ce05e96be1d17
johntduong <johntduong@yahoo.com>
2018-01-10 16:11:03 -0800

    modified search component and created ShowCard component

---
+++ courses/complete-intro-to-react/js/ShowCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ShowCard = props => (
+  <div className="show-card">
+    <img
+      alt={`${props.show.title} Show Poster`}
+      src={`/public/img/posters/${props.show.poster}`}
+    />
+    <div>
+      <h3>{props.show.title}</h3>
+      <h4>({props.show.year})</h4>
+      <p>{props.show.description}</p>
+      <h1>{Math.random()}</h1>
+    </div>
+  </div>
+);
+
+export default ShowCard;

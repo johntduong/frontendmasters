5378bdf1f00b0e2efe9061bea235ab7fb5e1e054
johntduong <johntduong@yahoo.com>
2018-01-11 17:31:21 -0800

    added babel-eslint parser for ESLint and removed constructor

--- courses/complete-intro-to-react/js/Search.jsx
+++ courses/complete-intro-to-react/js/Search.jsx
@@ -16,20 +16,23 @@ import preload from "../data.json";
 // );
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchTerm: "this is some sort of debug statement"
-    };
-  }
-
+  state = {
+    searchTerm: ""
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
   render() {
     return (
       <div className="search">
         <header>
           <h1>svideo</h1>
-          <input type="text" placeholder="search" />
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="search"
+          />
         </header>
         <div>
           {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}

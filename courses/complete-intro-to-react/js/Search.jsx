2a8837a72f238f69e3030acb343f518a76916d00
johntduong <johntduong@yahoo.com>
2018-01-12 23:52:58 -0800

    removed old code from search component

--- courses/complete-intro-to-react/js/Search.jsx
+++ courses/complete-intro-to-react/js/Search.jsx
@@ -2,19 +2,6 @@ import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-// Stateless functional component
-// const Search = () => (
-//   <div className="search">
-//   <header>
-//     <h1>svideo</h1>
-//     <input type="text" placeholder="search" />
-//   </header>
-//     <div>
-//       {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-//     </div>
-//   </div>
-// );
-
 class Search extends Component {
   state = {
     searchTerm: ""

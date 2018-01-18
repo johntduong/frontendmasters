ee870d7c51ca731ac5e92fb2b9939bdb4bdcee8e
johntduong <johntduong@yahoo.com>
2018-01-17 21:39:47 -0800

    added flow, custom typed definitions, upgraded babel-eslint package, and added annotations

--- courses/complete-intro-to-react/js/Search.jsx
+++ courses/complete-intro-to-react/js/Search.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
@@ -6,19 +8,21 @@ class Search extends Component {
   state = {
     searchTerm: ""
   };
-  handleSearchTermChange = event => {
+  handleSearchTermChange = (
+    event: SyntheticKeyboardEvent & { target: HTMLInputElement }
+  ) => {
     this.setState({ searchTerm: event.target.value });
   };
   render() {
     return (
       <div className="search">
         <header>
-          <h1>something else</h1>
+          <h1>svideo</h1>
           <input
             onChange={this.handleSearchTermChange}
             value={this.state.searchTerm}
             type="text"
-            placeholder="search"
+            placeholder="Search"
           />
         </header>
         <div>
